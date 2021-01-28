be61a702598cfd7960bfcb08ba7de7eaf85c5f95
Ahmed Hany <ahmedhany70@outlook.com>
2021-01-28 16:46:08 +0200

    v1.3 new season for sleep

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # battery Level
 
+## Icon
+
 App gives you numbric Battery icon like this
 
 ![](images/icon.png)
+
+Updated real time with only less than 6 mb RAM usage
+
+## History
+
+with history of your battery usage
+
+![](images/history.png)
+
+seasons doesn't count if laptop turned off
+
+## Notifications
+
+Notifications when battery below 40% / above 80%
+
+![](images/notification.png)
+
+So you notice and connect/disconnect charger
+
+TODO :  make notification percent customizable
--- battery.level/TrayIcon.cs
+++ battery.level/TrayIcon.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.IO;
 using Microsoft.Win32;
@@ -10,50 +9,68 @@ namespace battery.level
     class TrayIcon
     {
         private int oldPercentage;
-        private NotifyIcon notifyIcon;
+        public NotifyIcon notifyIcon;
         public static string logpath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData) + @"\BatteryLevelLog.txt";
+        RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
         public TrayIcon()
         {
-            ContextMenu contextMenu = new ContextMenu();
-            MenuItem menuItemExit = new MenuItem(), menuItemStartup = new MenuItem(), menuItemHistory = new MenuItem(), menuItemLog = new MenuItem();
-            notifyIcon = new NotifyIcon();
-            // initialize contextMenu
-            contextMenu.MenuItems.AddRange(new MenuItem[] { menuItemExit, menuItemHistory, menuItemStartup, menuItemLog });
             // initialize menuItem
-            menuItemExit.Index = 0;
-            menuItemExit.Text = "Exit";
+            MenuItem menuItemExit = new MenuItem() { Index = 0, Text = "Exit" };
             menuItemExit.Click += new System.EventHandler(menuItem_Click);
-            menuItemHistory.Index = 1;
-            menuItemHistory.Text = "History";
+
+            MenuItem menuItemHistory = new MenuItem() { Index = 1, Text = "History" };
             menuItemHistory.Click += new EventHandler(menuItemHistory_Click);
-            menuItemStartup.Index = 2;
-            menuItemStartup.Text = "Run at Startup";
+
+            MenuItem menuItemStartup = new MenuItem() { Index = 2, Text = "Run at Startup" };
+            menuItemStartup.Checked = (rk.GetValue(Application.ProductName) != null && rk.GetValue(Application.ProductName).ToString() == Application.ExecutablePath);
             menuItemStartup.Click += new EventHandler(menuItemStartup_Click);
-            menuItemLog.Index = 3;
-            menuItemLog.Text = "Log";
+
+            MenuItem menuItemLog = new MenuItem() { Index = 3, Text = "Log" };
             menuItemLog.Click += new EventHandler(MenuItemLog_Click);
 
-            RegistryKey rk = Registry.CurrentUser.OpenSubKey
-                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-            if (rk.GetValue(Application.ProductName) != null && rk.GetValue(Application.ProductName).ToString() == Application.ExecutablePath)
-            {
-                menuItemStartup.Checked = true;
-            }
-            else
-                menuItemStartup.Checked = false;
+            // initialize contextMenu
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.MenuItems.AddRange(new MenuItem[] { menuItemExit, menuItemHistory, menuItemStartup, menuItemLog });
 
+            // initialize NotifyIcon
+            notifyIcon = new NotifyIcon();
             notifyIcon.ContextMenu = contextMenu;
             notifyIcon.Visible = true;
             notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+
+            //new season
             File.AppendAllText(logpath, "" + Environment.NewLine);
+
             //timer
             Timer timer = new Timer();
             timer.Tick += new EventHandler(timer_Tick);
             timer.Interval = 1000; // in miliseconds
             timer.Start();
+
+            //new season for sleep event
+            SystemEvents.PowerModeChanged += OnPowerChange;
+        }
+        private void OnPowerChange(object s, PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Suspend:
+                    File.AppendAllText(logpath, "" + Environment.NewLine);
+                    logbattery();
+                    break;
+                case PowerModes.Resume:
+                    logbattery();
+                    File.AppendAllText(logpath, "" + Environment.NewLine);
+                    break;
+            }
         }
 
         private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+        }
+        private void menuItemHistory_Click(object sender, EventArgs e)
         {
             Form1 frm = new Form1();
             frm.Show();
@@ -64,53 +81,48 @@ namespace battery.level
             System.Diagnostics.Process.Start(logpath);
         }
 
+        private void logbattery()
+        {
+            string Percent = Convert.ToInt32(SystemInformation.PowerStatus.BatteryLifePercent * 100).ToString();
+            File.AppendAllText(logpath, Percent + "\t" + DateTime.Now.ToShortTimeString() + "\t" + DateTime.Now.ToShortDateString() + Environment.NewLine);
+        }
+
         private void timer_Tick(object sender, EventArgs e)
         {
             PowerStatus powerStatus = SystemInformation.PowerStatus;
-            int batteryPercentage = Convert.ToInt32(powerStatus.BatteryLifePercent * 100);
-            if (oldPercentage != batteryPercentage)
-            {
-                oldPercentage = batteryPercentage;
+            int Percent = Convert.ToInt32(powerStatus.BatteryLifePercent * 100);
+            if (oldPercentage == Percent)
+                return;
 
-                Brush textBrush = new SolidBrush(Color.White);
-                Bitmap image = new Bitmap(16, 16);
-                Graphics g = Graphics.FromImage(image);
+            oldPercentage = Percent;
+            logbattery();
+
+            //icon draw
+            string text = (Percent == 100) ? " F" : (Percent < 10) ? "  " + Percent.ToString() : Percent.ToString();
+
+            Bitmap image = new Bitmap(16, 16);
+
+            using (Graphics g = Graphics.FromImage(image))
+            {
                 g.Clear(Color.Transparent);
                 g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
-                string text = batteryPercentage.ToString();
-                if (batteryPercentage == 100)
-                {
-                    text = " F";
-                }
-                else if (batteryPercentage < 10)
-                {
-                    text = "  " + text;
-                }
-                g.DrawString(text, new Font("Microsoft Sans Serif", 16, FontStyle.Regular, GraphicsUnit.Pixel), textBrush, -4, -2);
+                g.DrawString(text, new Font("Microsoft Sans Serif", 16, FontStyle.Regular, GraphicsUnit.Pixel), new SolidBrush(Color.White), -4, -2);
+            }
 
-                System.IntPtr intPtr = image.GetHicon();
-                notifyIcon.Icon = Icon.FromHandle(intPtr);
+            notifyIcon.Icon = Icon.FromHandle(image.GetHicon());
 
-                File.AppendAllText(logpath, batteryPercentage.ToString() + "\t" + DateTime.Now.ToShortTimeString() + "\t" + DateTime.Now.ToShortDateString() + Environment.NewLine);
-
-                if(batteryPercentage >= 80)
-                {
-                    if(powerStatus.PowerLineStatus == PowerLineStatus.Online)
-                    {
-                        notifyIcon.BalloonTipText = "Release Charger for Better Battery life";
-                        notifyIcon.BalloonTipTitle = "Battery Charged";
-                        notifyIcon.ShowBalloonTip(3);
-                    }
-                }
-                else if (batteryPercentage <= 40)
-                {
-                    if (powerStatus.PowerLineStatus == PowerLineStatus.Offline)
-                    {
-                        notifyIcon.BalloonTipText = "Connect Charger for Better Battery life";
-                        notifyIcon.BalloonTipTitle = "Battery Finished";
-                        notifyIcon.ShowBalloonTip(3);
-                    }
-                }
+            //notifications
+            if (Percent >= 80 && powerStatus.PowerLineStatus == PowerLineStatus.Online)
+            {
+                notifyIcon.BalloonTipText = "Release Charger for Better Battery life";
+                notifyIcon.BalloonTipTitle = "Battery Charged";
+                notifyIcon.ShowBalloonTip(3);
+            }
+            else if (Percent <= 40 && powerStatus.PowerLineStatus == PowerLineStatus.Offline)
+            {
+                notifyIcon.BalloonTipText = "Connect Charger for Better Battery life";
+                notifyIcon.BalloonTipTitle = "Battery Finished";
+                notifyIcon.ShowBalloonTip(3);
             }
         }
         private void menuItem_Click(object sender, EventArgs e)
@@ -119,21 +131,15 @@ namespace battery.level
             notifyIcon.Dispose();
             Application.Exit();
         }
-        private void menuItemHistory_Click(object sender, EventArgs e)
-        {
-            Form1 frm = new Form1();
-            frm.Show();
-        }
+        
         private void menuItemStartup_Click(object sender, EventArgs e)
         {
             MenuItem m = (MenuItem)sender;
-            m.Checked = !m.Checked;
-                RegistryKey rk = Registry.CurrentUser.OpenSubKey
-                    ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-                if (m.Checked)
-                    rk.SetValue(Application.ProductName, Application.ExecutablePath);
-                else
-                    rk.DeleteValue(Application.ProductName, false);
+            if (m.Checked)
+                rk.DeleteValue(Application.ProductName, false);
+            else
+                rk.SetValue(Application.ProductName, Application.ExecutablePath);
+            m.Checked = (rk.GetValue(Application.ProductName) != null && rk.GetValue(Application.ProductName).ToString() == Application.ExecutablePath);
         }
     }
 }
